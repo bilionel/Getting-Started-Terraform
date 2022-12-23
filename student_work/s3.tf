2a2fba65baf5fa6bee17ab339b7f584b659af2f5
Lionel Tsimi Biloa <ltsimibiloa@yext.com>
2022-12-23 20:07:44 +0100

    m8: use s3 web app module to create s3 bucket and objects

--- student_work/s3.tf
+++ student_work/s3.tf
@@ -1,65 +1,10 @@
 ## aws_s3_bucket
-resource "aws_s3_bucket" "web_bucket" {
-  bucket        = local.s3_bucket_name
-  force_destroy = true
+module "web_app_s3" {
+  source = "./modules/globo-web-app-s3"
 
-  tags = local.common_tags
-
-}
-
-## aws_s3_bucket_policy
-resource "aws_s3_bucket_policy" "allow_access_to_s3_bucket" {
-  bucket = aws_s3_bucket.web_bucket.id
-  policy = data.aws_iam_policy_document.allow_access_to_s3_bucket.json
-}
-
-data "aws_iam_policy_document" "allow_access_to_s3_bucket" {
-  statement {
-    principals {
-      type        = "AWS"
-      identifiers = ["${data.aws_elb_service_account.root.arn}"]
-    }
-
-    actions = ["s3:PutObject"]
-
-    resources = ["arn:aws:s3:::${local.s3_bucket_name}/alb-logs/*"]
-
-  }
-
-  statement {
-    principals {
-      type        = "Service"
-      identifiers = ["delivery.logs.amazonaws.com"]
-    }
-
-    actions = ["s3:PutObject"]
-
-    resources = ["arn:aws:s3:::${local.s3_bucket_name}/alb-logs/*"]
-
-    condition {
-      test     = "StringLike"
-      variable = "s3:x-amz-acl"
-      values   = ["bucket-owner-full-control"]
-    }
-  }
-
-  statement {
-    principals {
-      type        = "Service"
-      identifiers = ["delivery.logs.amazonaws.com"]
-    }
-
-    actions = ["s3:GetBucketAcl"]
-
-    resources = ["arn:aws:s3:::${local.s3_bucket_name}"]
-  }
-}
-
-
-## aws_s3_bucket_acl
-resource "aws_s3_bucket_acl" "web_bucket_acl" {
-  bucket = aws_s3_bucket.web_bucket.id
-  acl    = "private"
+  bucket_name             = local.s3_bucket_name
+  elb_service_account_arn = data.aws_elb_service_account.root.arn
+  common_tags             = local.common_tags
 }
 
 ## aws_s3_object
@@ -68,66 +13,10 @@ resource "aws_s3_object" "website_content" {
     website = "/website/index.html"
     logo    = "/website/Globo_logo_Vert.png"
   }
-  bucket = aws_s3_bucket.web_bucket.bucket
+  bucket = module.web_app_s3.bucket.id
   key    = each.value
   source = ".${each.value}"
 
   tags = local.common_tags
 
-}
-
-## aws_iam_role
-resource "aws_iam_role" "allow_nginx_s3" {
-  name = "${local.name_prefix}-allow_nginx_s3"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-
-  tags = local.common_tags
-}
-
-## aws_iam_role_policy
-resource "aws_iam_role_policy" "allow_s3_all" {
-  name = "${local.name_prefix}-allow_s3_all"
-  role = aws_iam_role.allow_nginx_s3.name
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "s3:*"
-      ],
-      "Effect": "Allow",
-      "Resource": [
-                "arn:aws:s3:::${local.s3_bucket_name}",
-                "arn:aws:s3:::${local.s3_bucket_name}/*"
-            ]
-    }
-  ]
-}
-EOF
-
-}
-
-## aws_iam_instance_profile
-resource "aws_iam_instance_profile" "nginx_profile" {
-  name = "${local.name_prefix}-nginx_profile"
-  role = aws_iam_role.allow_nginx_s3.name
-
-  tags = local.common_tags
 }
